7d658ebbc361b9fcc753f32db689aff4bfeea731
Demilicz <Necrolick92@gmail.com>
2021-12-27 16:42:30 +0100

    last commit, now it's done

--- pages/[prediction].tsx
+++ pages/[prediction].tsx
@@ -7,12 +7,23 @@ import Head from 'next/head';
 
 import Header from './components/header';
 import Footer from './components/footer';
+import Image from 'next/image';
+
+
 
 const contextHandler = (con: string) => {
   let sym = con.indexOf('&');
   let sign = con.slice(0, sym);
   let day = con.slice(sym + 1);
   return {sign, day }
+}
+
+const pathHandler = (con: string) => {
+  let sym = con.indexOf('&');
+  let sign = con.slice(0, sym);
+  let upperSign = sign[0].toUpperCase() + sign.substring(1);;
+
+  return upperSign;
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
@@ -53,25 +64,69 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   return {
     props: {
-      prediction: data
+      prediction: data,
+      getPath: link
     }
   }
 
 }
 
-export const Prediction : NextPage = ({ prediction }: InferGetStaticPropsType<typeof getStaticProps>) => {
+export const Prediction : NextPage = ({ prediction, getPath }: InferGetStaticPropsType<typeof getStaticProps>) => {
+
   console.log(prediction);
 
+
+
   return (
-    <div>
+    <div >
        <Head>
           <title>Prediction | Horoscope</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Header/>
-      <div>{prediction.description}</div>
-      <div>{prediction.date_range}</div>
+      <div className='container'>
+        <div className='upperContext'>
+          <Image src={`/${pathHandler(getPath)}.png`} width={350} height={350} alt={`${pathHandler(getPath)}`}/>
+          <h1 className='title'>{pathHandler(getPath)}</h1>
+          <h3>{prediction.date_range}</h3>
+        </div>
+        <p className='data'>{'Current date: ' + prediction.current_date}</p>
+        <p className='description'>{prediction.description}</p>
+        <p className='text'>{`Lucky number on this day is ${prediction.lucky_number}. Your mood on this day is ${prediction.mood}. Today you compatible with ${prediction.compatibility}.`}</p>
+      </div>
       <Footer/>
+        <style jsx>{`
+            .container {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              flex-wrap: wrap;
+              flex-direction:column;
+            }
+            .upperContext{
+              margin: 60px 0;
+              display: flex;
+              flex-direction:column;
+              align-items: center;
+            }
+            .title{
+              font-size: 28px;
+              margin-bottom:12px;
+              margin-top: 28px;
+            }
+            .data {
+              font-size: 18px;
+              font-weight: 500;
+              margin-bottom: 50px;
+            }
+            .description {
+              font-size: 22px;
+            }
+            .text {
+              font-size: 22px;
+              margin-bottom: 30px;
+            }
+        `}</style>
     </div>
   )
 }
